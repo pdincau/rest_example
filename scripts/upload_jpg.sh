b48b4bb6cdd8cd7dd320e1f0cb03d6917bd58048
Paolo D'Incau <paolo.dincau@xpeppers.com>
2013-12-28 14:41:32 +0100

    Adapting scripts to support authentication

--- scripts/upload_jpg.sh
+++ scripts/upload_jpg.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -v -X POST -H "content-type: image/jpg" --upload-file erlang http://localhost:8080/images
+curl -v -X POST -H "content-type: image/jpg" --upload-file erlang http://localhost:8080/images?user_token=myusertoken
 
--- scripts/upload_png.sh
+++ scripts/upload_png.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl -v -X POST -H "content-type: image/png" --upload-file erlang_logo http://localhost:8080/images
+curl -v -X POST -H "content-type: image/png" --upload-file erlang_logo http://localhost:8080/images?user_token=myusertoken
 
